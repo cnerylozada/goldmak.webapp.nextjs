812fe37e5fd7bb76e9ecbca37c7a1d2f5d8655a2
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2024-06-29 11:42:55 -0500

    feat: files validation were added

--- modules/product/CreateProductForm.tsx
+++ modules/product/CreateProductForm.tsx
@@ -1,14 +1,16 @@
 "use client";
 import { ProductType, productSchema } from "@/validations/products";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect } from "react";
+import Image from "next/image";
+import { useCallback, useEffect, useState } from "react";
+import { useDropzone } from "react-dropzone";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 export const CreateProductForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
     reset,
   } = useForm<ProductType>({
     mode: "all",
@@ -18,6 +20,32 @@ export const CreateProductForm = () => {
   useEffect(() => {
     reset({ name: "samsung", stock: 1, price: 45.69 });
   }, []);
+
+  const [userAcceptedFiles, setUserAcceptedFiles] = useState<File[]>([]);
+  const onDrop = useCallback((acceptedFiles) => {
+    setUserAcceptedFiles((_) => [..._, ...(acceptedFiles as File[])]);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive, fileRejections } =
+    useDropzone({
+      onDrop,
+      accept: {
+        "image/png": [".png", ".jpg", ".jpeg"],
+      },
+      maxFiles: 5,
+      maxSize: 800000,
+    });
+  const fileRejectionItems = fileRejections.map(({ file, errors }, index) => {
+    return (
+      <div key={index} className="text-red-600">
+        {file.name} - {file.size * 0.001} KB
+        <div>
+          {errors.map((e) => (
+            <div key={e.code}>{e.message}</div>
+          ))}
+        </div>
+      </div>
+    );
+  });
 
   const onSubmit: SubmitHandler<ProductType> = (data) => {
     const product = { ...data, price: +data.price };
@@ -38,7 +66,50 @@ export const CreateProductForm = () => {
         <input type="number" {...register("stock")} />
         {errors.stock && <div>{errors.stock.message}</div>}
 
-        <input type="submit" />
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <p>Drop the files here ...</p>
+          ) : (
+            <p>Drag 'n' drop some files here, or click to select files</p>
+          )}
+        </div>
+
+        <div>
+          {!!userAcceptedFiles.length &&
+            userAcceptedFiles.map((_, index) => (
+              <div key={index}>
+                <div> size: {_.size * 0.001} KB</div>
+                <div>
+                  <button
+                    onClick={() => {
+                      setUserAcceptedFiles((files) =>
+                        files.filter((item, itemIndex) => itemIndex !== index)
+                      );
+                    }}
+                  >
+                    Remove
+                  </button>
+                </div>
+                <Image
+                  src={URL.createObjectURL(_)}
+                  alt={URL.createObjectURL(_)}
+                  width={100}
+                  height={100}
+                />
+              </div>
+            ))}
+
+          {fileRejectionItems}
+        </div>
+
+        <button
+          type="submit"
+          disabled={!isValid || !userAcceptedFiles.length}
+          className="bg-green-400 disabled:bg-gray-300"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
