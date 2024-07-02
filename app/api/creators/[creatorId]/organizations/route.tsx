bfa171f0a7ea8d44f1d9736325ef39c15554d8bf
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2024-07-02 10:36:10 -0500

    feat: queryparams were added in organizations by creator route-handler

--- app/api/creators/[creatorId]/organizations/route.tsx
+++ app/api/creators/[creatorId]/organizations/route.tsx
@@ -6,6 +6,11 @@ export async function GET(
   request: NextRequest,
   { params }: { params: { creatorId: string } }
 ) {
+  const searchParams = request.nextUrl.searchParams;
+  const page = searchParams.get("page") ? +searchParams.get("page")! : 0;
+  const PAGE_SIZE = 2;
+
+  const offset = (page - 1) * PAGE_SIZE;
   const dataResponse = await prisma.organization.findMany({
     where: {
       userId: {
@@ -19,8 +24,8 @@ export async function GET(
         },
       },
     },
-    skip: 0,
-    take: 2,
+    skip: offset,
+    take: PAGE_SIZE,
   });
 
   return NextResponse.json(dataResponse, { status: 201 });
--- modules/organization/ListOrganizations.tsx
+++ modules/organization/ListOrganizations.tsx
@@ -1,35 +1,47 @@
 "use client";
 
+import { Organization } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
 
 export const ListOrganizations = ({ creatorId }: { creatorId: string }) => {
   const getData = async () => {
-    const response = await fetch(`/api/creators/${creatorId}/organizations`);
+    const response = await fetch(
+      `/api/creators/${creatorId}/organizations?page=1`
+    );
     const data = await response.json();
     return data;
   };
 
-  const { data } = useQuery({ queryKey: ["organizations"], queryFn: getData });
+  const { data } = useQuery({
+    queryKey: [`${creatorId}-organizations`],
+    queryFn: getData,
+  });
 
   console.log(data);
   return (
     <div>
-      {/* {dataResponse!.map((_) => (
-          <div key={_.id}>
-            <div>{_.id}</div>
-            <div>{_.name}</div>
-            <div>
-              <Image
-                src={`${_.resourceFiles[0].bucketKey}`}
-                width={180}
-                height={37}
-                alt={_.resourceFiles[0].bucketKey}
-              />
-            </div>
-            <div>{_.description}</div>
-            <div>{_.createdAt.toString()}</div>
-          </div>
-        ))} */}
+      <div>
+        {!!data &&
+          data.map(
+            (_: Organization & { resourceFiles: { bucketKey: string }[] }) => (
+              <div key={_.id}>
+                <div>{_.id}</div>
+                <div>{_.name}</div>
+                <div>
+                  <Image
+                    src={`${_.resourceFiles[0].bucketKey}`}
+                    width={180}
+                    height={37}
+                    alt={_.resourceFiles[0].bucketKey}
+                  />
+                </div>
+                <div>{_.description}</div>
+                <div>{_.createdAt.toString()}</div>
+              </div>
+            )
+          )}
+      </div>
     </div>
   );
 };
