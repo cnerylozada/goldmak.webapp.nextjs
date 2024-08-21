0cdf0020edd74862dc738244789332e742fe6682
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2024-08-21 13:01:41 -0500

    chore: test

--- modules/Mock.tsx
+++ modules/Mock.tsx
@@ -4,12 +4,14 @@ export const Mock = () => {
     <div>
       <button
         onClick={() => {
-          (window as any).fbq.track("CompleteRegistration", {
-            content_id: "12345",
-            content_type: "product",
-            value: 29.99,
-            currency: "USD",
-          });
+          window.fbq("track", "CompleteRegistration");
+          //   (window as any).fbq.track("CompleteRegistration", {
+          //     content_id: "12345",
+          //     content_type: "product",
+          //     value: 29.99,
+          //     currency: "USD",
+          //   });
+          // }}
         }}
       >
         button
